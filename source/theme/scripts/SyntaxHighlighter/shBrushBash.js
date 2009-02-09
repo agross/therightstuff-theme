551067827fe869b36686b81a60a5fe2085484ba8
agross <agross@0039f234-ce90-cf4f-8172-5c9ff3ac8660>
2009-02-09 02:07:03 +0000

    Upgrading to SyntaxHighlighter 2.0
    git-svn-id: https://svn.therightstuff.de:8443/svn/agross/Public/dasBlog/The%20Right%20Stuff%20Theme/trunk@1003 0039f234-ce90-cf4f-8172-5c9ff3ac8660

--- source/theme/scripts/SyntaxHighlighter/shBrushBash.js
+++ source/theme/scripts/SyntaxHighlighter/shBrushBash.js
@@ -1,66 +1,51 @@
 /**
- *  Author: Saúl Martínez Vidals
- *  http://buglog-saul.blogspot.com/
+ * SyntaxHighlighter
+ * http://alexgorbatchev.com/
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, version 3 of the License.
+ * @version
+ * 2.0.287 (February 06 2009)
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * @author
+ * Alex Gorbatchev
+ * 
+ * @copyright
+ * Copyright (C) 2004-2009 Alex Gorbatchev.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Licensed under a GNU Lesser General Public License.
+ * http://creativecommons.org/licenses/LGPL/2.1/
+ *
+ * SyntaxHighlighter is donationware. You are allowed to download, modify and distribute 
+ * the source code in accordance with LGPL 2.1 license, however if you want to use 
+ * SyntaxHighlighter on your site or include it in your product, you must donate.
+ * http://alexgorbatchev.com/wiki/SyntaxHighlighter:Donate
  */
-
-/**
- * Changes by Fred Wu:
- *
- * [2008-06-28]
- * + added 'GetKeywords_without' to the shCore.js file to fix Bash keywords and variables conflict
- */
-dp.sh.Brushes.Bash = function()
+SyntaxHighlighter.brushes.Bash = function()
 {
-    var builtins =  'alias bg bind break builtin cd command compgen complete continue ' +
-                    'declare dirs disown echo enable eval exec exit export fc fg ' +
-                    'getopts hash help history jobs kill let local logout popd printf ' +
-                    'pushd pwd read readonly return set shift shopt source ' +
-                    'suspend test times trap type typeset ulimit umask unalias unset wait';
-
-    var keywords =  'case do done elif else esac fi for function if in select then ' +
-                    'time until while';
-
-    this.regexList = [
-                    /* cometarios */
-                    {regex: dp.sh.RegexLib.SingleLinePerlComments, css: 'comment'},
-                    /* cadenas de texto */
-                    {regex: dp.sh.RegexLib.DoubleQuotedString, css: 'string'},
-                    {regex: dp.sh.RegexLib.SingleQuotedString, css: 'string'},
-                    /* delimitadores */
-                    {regex: new RegExp('[()[\\]{}]', 'g'), css: 'delim'},
-                    /* variables */
-                    {regex: new RegExp('\\$\\w+', 'g'), css: 'vars'},
-                    {regex: new RegExp('\\w+=', 'g'), css: 'vars'},
-                    /* banderas */
-                    {regex: new RegExp('\\s-\\w+', 'g'), css: 'flag'},
-                    /* builtins */
-                    {regex: new RegExp(this.GetKeywords(builtins), 'gm'), css: 'builtin'},
-                    /* palabras reservadas */
-                    {regex: new RegExp(this.GetKeywords_without(keywords, '='), 'gm'), css: 'keyword'}
-                    ];
-
-    this.CssClass = 'dp-bash';
-
-    this.Style =    '.dp-bash .builtin {color: maroon; font-weight: bold;}' +
-                    '.dp-bash .comment {color: gray;}' +
-                    '.dp-bash .delim {font-weight: bold;}' +
-                    '.dp-bash .flag {color: green;}' +
-                    '.dp-bash .string {color: red;}' +
-                    '.dp-bash .vars {color: blue;}';
-
+	var keywords =	'if fi then elif else for do done until while break continue case function return in eq ne gt lt ge le';
+	var commands =  'alias apropos awk bash bc bg builtin bzip2 cal cat cd cfdisk chgrp chmod chown chroot' +
+					'cksum clear cmp comm command cp cron crontab csplit cut date dc dd ddrescue declare df ' +
+					'diff diff3 dig dir dircolors dirname dirs du echo egrep eject enable env ethtool eval ' +
+					'exec exit expand export expr false fdformat fdisk fg fgrep file find fmt fold format ' +
+					'free fsck ftp gawk getopts grep groups gzip hash head history hostname id ifconfig ' +
+					'import install join kill less let ln local locate logname logout look lpc lpr lprint ' +
+					'lprintd lprintq lprm ls lsof make man mkdir mkfifo mkisofs mknod more mount mtools ' +
+					'mv netstat nice nl nohup nslookup open op passwd paste pathchk ping popd pr printcap ' +
+					'printenv printf ps pushd pwd quota quotacheck quotactl ram rcp read readonly renice ' +
+					'remsync rm rmdir rsync screen scp sdiff sed select seq set sftp shift shopt shutdown ' +
+					'sleep sort source split ssh strace su sudo sum symlink sync tail tar tee test time ' +
+					'times touch top traceroute trap tr true tsort tty type ulimit umask umount unalias ' +
+					'uname unexpand uniq units unset unshar useradd usermod users uuencode uudecode v vdir ' +
+					'vi watch wc whereis which who whoami Wget xargs yes'
+					;
+    
+	this.regexList = [
+		{ regex: SyntaxHighlighter.regexLib.singleLinePerlComments,		css: 'comments' },		// one line comments
+		{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,			css: 'string' },		// double quoted strings
+		{ regex: new RegExp(this.getKeywords(keywords), 'gm'),			css: 'keyword' },		// keywords
+		{ regex: new RegExp(this.getKeywords(commands), 'gm'),			css: 'functions' }		// commands
+		];
 }
 
-dp.sh.Brushes.Bash.prototype = new dp.sh.Highlighter();
-dp.sh.Brushes.Bash.Aliases = ['bash', 'sh'];
+SyntaxHighlighter.brushes.Bash.prototype	= new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Bash.aliases		= ['bash', 'shell'];
+
--- source/theme/scripts/SyntaxHighlighter/shBrushDelphi.js
+++ source/theme/scripts/SyntaxHighlighter/shBrushDelphi.js
@@ -1,10 +1,49 @@
-/*
- * JsMin
- * Javascript Compressor
- * http://www.crockford.com/
- * http://www.smallsharptools.com/
-*/
+/**
+ * SyntaxHighlighter
+ * http://alexgorbatchev.com/
+ *
+ * @version
+ * 2.0.287 (February 06 2009)
+ *
+ * @author
+ * Alex Gorbatchev
+ * 
+ * @copyright
+ * Copyright (C) 2004-2009 Alex Gorbatchev.
+ *
+ * Licensed under a GNU Lesser General Public License.
+ * http://creativecommons.org/licenses/LGPL/2.1/
+ *
+ * SyntaxHighlighter is donationware. You are allowed to download, modify and distribute 
+ * the source code in accordance with LGPL 2.1 license, however if you want to use 
+ * SyntaxHighlighter on your site or include it in your product, you must donate.
+ * http://alexgorbatchev.com/wiki/SyntaxHighlighter:Donate
+ */
+SyntaxHighlighter.brushes.Delphi = function()
+{
+	var keywords =	'abs addr and ansichar ansistring array as asm begin boolean byte cardinal ' +
+					'case char class comp const constructor currency destructor div do double ' +
+					'downto else end except exports extended false file finalization finally ' +
+					'for function goto if implementation in inherited int64 initialization ' +
+					'integer interface is label library longint longword mod nil not object ' +
+					'of on or packed pansichar pansistring pchar pcurrency pdatetime pextended ' +
+					'pint64 pointer private procedure program property pshortstring pstring ' +
+					'pvariant pwidechar pwidestring protected public published raise real real48 ' +
+					'record repeat set shl shortint shortstring shr single smallint string then ' +
+					'threadvar to true try type unit until uses val var varirnt while widechar ' +
+					'widestring with word write writeln xor';
 
-dp.sh.Brushes.Delphi=function()
-{var keywords='abs addr and ansichar ansistring array as asm begin boolean byte cardinal '+'case char class comp const constructor currency destructor div do double '+'downto else end except exports extended false file finalization finally '+'for function goto if implementation in inherited int64 initialization '+'integer interface is label library longint longword mod nil not object '+'of on or packed pansichar pansistring pchar pcurrency pdatetime pextended '+'pint64 pointer private procedure program property pshortstring pstring '+'pvariant pwidechar pwidestring protected public published raise real real48 '+'record repeat set shl shortint shortstring shr single smallint string then '+'threadvar to true try type unit until uses val var varirnt while widechar '+'widestring with word write writeln xor';this.regexList=[{regex:new RegExp('\\(\\*[\\s\\S]*?\\*\\)','gm'),css:'comment'},{regex:new RegExp('{(?!\\$)[\\s\\S]*?}','gm'),css:'comment'},{regex:dp.sh.RegexLib.SingleLineCComments,css:'comment'},{regex:dp.sh.RegexLib.SingleQuotedString,css:'string'},{regex:new RegExp('\\{\\$[a-zA-Z]+ .+\\}','g'),css:'directive'},{regex:new RegExp('\\b[\\d\\.]+\\b','g'),css:'number'},{regex:new RegExp('\\$[a-zA-Z0-9]+\\b','g'),css:'number'},{regex:new RegExp(this.GetKeywords(keywords),'gm'),css:'keyword'}];this.CssClass='dp-delphi';this.Style='.dp-delphi .number { color: blue; }'+'.dp-delphi .directive { color: #008284; }'+'.dp-delphi .vars { color: #000; }';}
-dp.sh.Brushes.Delphi.prototype=new dp.sh.Highlighter();dp.sh.Brushes.Delphi.Aliases=['delphi','pascal'];
+	this.regexList = [
+		{ regex: /\(\*[\s\S]*?\*\)/gm,								css: 'comments' },  	// multiline comments (* *)
+		{ regex: /{(?!\$)[\s\S]*?}/gm,								css: 'comments' },  	// multiline comments { }
+		{ regex: SyntaxHighlighter.regexLib.singleLineCComments,	css: 'comments' },  	// one line
+		{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },		// strings
+		{ regex: /\{\$[a-zA-Z]+ .+\}/g,								css: 'color1' },		// compiler Directives and Region tags
+		{ regex: /\b[\d\.]+\b/g,									css: 'value' },			// numbers 12345
+		{ regex: /\$[a-zA-Z0-9]+\b/g,								css: 'value' },			// numbers $F5D3
+		{ regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword' }		// keyword
+		];
+};
+
+SyntaxHighlighter.brushes.Delphi.prototype	= new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Delphi.aliases	= ['delphi', 'pascal'];

31d42c39de0ce70dc3381fca27871fb76d436acb
agross <agross@0039f234-ce90-cf4f-8172-5c9ff3ac8660>
2010-01-17 23:27:51 +0000

    Updating SyntaxHighlighter
    git-svn-id: https://svn.therightstuff.de:8443/svn/agross/Public/dasBlog/The%20Right%20Stuff%20Theme/trunk@1118 0039f234-ce90-cf4f-8172-5c9ff3ac8660

--- source/theme/scripts/SyntaxHighlighter/shBrushPowerShell.js
+++ source/theme/scripts/SyntaxHighlighter/shBrushPowerShell.js
@@ -1,3 +1,76 @@
+/**
+ * SyntaxHighlighter
+ * http://alexgorbatchev.com/
+ *
+ * SyntaxHighlighter is donationware. If you are using it, please donate.
+ * http://alexgorbatchev.com/wiki/SyntaxHighlighter:Donate
+ *
+ * @version
+ * 2.1.364 (October 15 2009)
+ * 
+ * @copyright
+ * Copyright (C) 2004-2009 Alex Gorbatchev.
+ *
+ * @license
+ * This file is part of SyntaxHighlighter.
+ * 
+ * SyntaxHighlighter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SyntaxHighlighter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SyntaxHighlighter.  If not, see <http://www.gnu.org/copyleft/lesser.html>.
+ */
+SyntaxHighlighter.brushes.PowerShell = function()
+{
+	// Contributes by B.v.Zanten, Getronics
+	// http://confluence.atlassian.com/display/CONFEXT/New+Code+Macro
 
-﻿SyntaxHighlighter.brushes.PowerShell=function()
-{var keywords='throw while until trap switch return ref process param in if global: function for finally filter end elseif else do default continue break begin \\? % #script #private #local #global';var operators='and as band bnot bor bxor casesensitive ccontains ceq cge cgt cle clike clt cmatch cne cnotcontains cnotlike cnotmatch contains creplace eq exact f file ge gt icontains ieq ige igt ile ilike ilt imatch ine inotcontains inotlike inotmatch ireplace is isnot le like lt match ne not notcontains notlike notmatch or regex replace wildcard';var verbs='Write Wrap Width Where Whatif Warning Wait Version Verify Verbose Value UserName URL Update Unlock Unique Uninstall TypeName TrustLevel Trusted Truncate Trace TimeStamp TimeOut TID Test Temp Tee Suspend Strict Stop Statistic State Start Split Speed SortBy Sort Size SID Shortname Set Send Select Scope SaveCred Role Retry Resume Restore Restart Resolve Resize Repair Rename Remove Regex Recurse Receive Reason ReadOnly Read Push Property Prompt Privilege Priority PrinterName PrincipalName PortName Pop Ping Password PassThru ParentID Parameter Owner Overwrite Output Out Operation Notify NewLine New Name Move Most Modified Minimum Migrate Merge Measure Maximum MacName LogName Log Lock Location LineCount Limit KeyLength KeyContainerName KeyAlgorithm Join JobName IpAddress Invoke Interval InterfaceName Interactive Install Insert Input Initialize Incremental Include Import Ignore Id Group Get From Format ForEach Force  Follow Filter FileName Fast Export Exclude Exact EventName ErrorLimit ErrorLevel Erase Encrypt Encoding Enable Elapsed DriveName Drain DomainName Disconnect Disable DirectoryName Description Descending Delete Default Debug Data Verbs CSPType CSPName Created Create Count Copy ConvertTo ConvertFrom Convert Continuous Connect Confirm ComputerName Compress Compatible Compare Command ClusterName Clear ClassName Checkpoint Char CertUsage CertSubjectName CertStoreLocation CertSerialNumber CertRequestFileName CertIssuerName CertFile CaseSensitive Cache BlockCount Binary Before AttributeName AssemblyName Ascending As ApplicationName Allocation All After Add ACL Accessed';this.regexList=[{regex:SyntaxHighlighter.regexLib.singleLinePerlComments,css:'comments'},{regex:new RegExp('@"\\n[\\s\\S]*?\\n"@','gm'),css:'string'},{regex:new RegExp("@'\\n[\\s\\S]*?\\n'@",'gm'),css:'string'},{regex:new RegExp('"(?:\\$\\([^\\)]*\\)|[^"]|`")*[^`]"','g'),css:'string'},{regex:new RegExp("'[^']*'",'g'),css:'string'},{regex:new RegExp('(?:'+verbs.replace(/ /g,'\\b|\\b')+'|[a-zA-Z_][a-zA-Z0-9_]*)-[a-zA-Z_][a-zA-Z0-9_]*','gmi'),css:'functions'},{regex:new RegExp('\\[[A-Z_\\[][A-Z0-9_. ,\\[\\]]*\\]*','gi'),css:'color1'},{regex:new RegExp('\\$(?:(?:global|script|private|env):)?[A-Z0-9_]+','gi'),css:'variable'},{regex:new RegExp(this.getKeywords(keywords),'gmi'),css:'keyword'},{regex:new RegExp('-'+this.getKeywords(operators),'gmi'),css:'color1'},{regex:new RegExp('\\s+-[a-zA-Z_][a-zA-Z0-9_]*','gmi'),css:'color1'}];};SyntaxHighlighter.brushes.PowerShell.prototype=new SyntaxHighlighter.Highlighter();SyntaxHighlighter.brushes.PowerShell.aliases=['powershell','posh','ps'];
+	var keywords = 'Add-Content Add-History Add-Member Add-PSSnapin Clear(-Content)? Clear-Item ' +
+				'Clear-ItemProperty Clear-Variable Compare-Object ConvertFrom-SecureString Convert-Path ' +
+				'ConvertTo-Html ConvertTo-SecureString Copy(-Item)? Copy-ItemProperty Export-Alias ' +
+				'Export-Clixml Export-Console Export-Csv ForEach(-Object)? Format-Custom Format-List ' +
+				'Format-Table Format-Wide Get-Acl Get-Alias Get-AuthenticodeSignature Get-ChildItem Get-Command ' +
+				'Get-Content Get-Credential Get-Culture Get-Date Get-EventLog Get-ExecutionPolicy ' +
+				'Get-Help Get-History Get-Host Get-Item Get-ItemProperty Get-Location Get-Member ' +
+				'Get-PfxCertificate Get-Process Get-PSDrive Get-PSProvider Get-PSSnapin Get-Service ' +
+				'Get-TraceSource Get-UICulture Get-Unique Get-Variable Get-WmiObject Group-Object ' +
+				'Import-Alias Import-Clixml Import-Csv Invoke-Expression Invoke-History Invoke-Item ' +
+				'Join-Path Measure-Command Measure-Object Move(-Item)? Move-ItemProperty New-Alias ' +
+				'New-Item New-ItemProperty New-Object New-PSDrive New-Service New-TimeSpan ' +
+				'New-Variable Out-Default Out-File Out-Host Out-Null Out-Printer Out-String Pop-Location ' +
+				'Push-Location Read-Host Remove-Item Remove-ItemProperty Remove-PSDrive Remove-PSSnapin ' +
+				'Remove-Variable Rename-Item Rename-ItemProperty Resolve-Path Restart-Service Resume-Service ' +
+				'Select-Object Select-String Set-Acl Set-Alias Set-AuthenticodeSignature Set-Content ' +
+				'Set-Date Set-ExecutionPolicy Set-Item Set-ItemProperty Set-Location Set-PSDebug ' +
+				'Set-Service Set-TraceSource Set(-Variable)? Sort-Object Split-Path Start-Service ' +
+				'Start-Sleep Start-Transcript Stop-Process Stop-Service Stop-Transcript Suspend-Service ' +
+				'Tee-Object Test-Path Trace-Command Update-FormatData Update-TypeData Where(-Object)? ' +
+				'Write-Debug Write-Error Write(-Host)? Write-Output Write-Progress Write-Verbose Write-Warning';
+	var alias = 'ac asnp clc cli clp clv cpi cpp cvpa diff epal epcsv fc fl ' +
+				'ft fw gal gc gci gcm gdr ghy gi gl gm gp gps group gsv ' +
+				'gsnp gu gv gwmi iex ihy ii ipal ipcsv mi mp nal ndr ni nv oh rdr ' +
+				'ri rni rnp rp rsnp rv rvpa sal sasv sc select si sl sleep sort sp ' +
+				'spps spsv sv tee cat cd cp h history kill lp ls ' +
+				'mount mv popd ps pushd pwd r rm rmdir echo cls chdir del dir ' +
+				'erase rd ren type % \\?';
+
+	this.regexList = [
+		{ regex: /#.*$/gm,										css: 'comments' },  // one line comments
+		{ regex: /\$[a-zA-Z0-9]+\b/g,							css: 'value'   },   // variables $Computer1
+		{ regex: /\-[a-zA-Z]+\b/g,								css: 'keyword' },   // Operators    -not  -and  -eq
+		{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,	css: 'string' },    // strings
+		{ regex: SyntaxHighlighter.regexLib.singleQuotedString,	css: 'string' },    // strings
+		{ regex: new RegExp(this.getKeywords(keywords), 'gmi'),	css: 'keyword' },
+		{ regex: new RegExp(this.getKeywords(alias), 'gmi'),	css: 'keyword' }
+	];
+};
+
+SyntaxHighlighter.brushes.PowerShell.prototype = new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.PowerShell.aliases = ['powershell', 'ps', 'posh'];

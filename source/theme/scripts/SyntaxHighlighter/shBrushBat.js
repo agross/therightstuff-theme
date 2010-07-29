6a194e71cc2ffc6771e0cbf03d62f860dc7bea56
agross <agross@0039f234-ce90-cf4f-8172-5c9ff3ac8660>
2010-07-29 18:54:22 +0000

    Adding syntax definitions for batch and yaml
    git-svn-id: https://svn.therightstuff.de:8443/svn/agross/Public/dasBlog/The%20Right%20Stuff%20Theme/trunk@1157 0039f234-ce90-cf4f-8172-5c9ff3ac8660

---
+++ source/theme/scripts/SyntaxHighlighter/shBrushBat.js
@@ -0,0 +1,103 @@
+;(function()
+{
+	// CommonJS
+	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+
+	function Brush()
+	{
+		var commands = 'ASSOC AT ATTRIB BREAK CACLS CD CHCP CHDIR CHKDSK CHKNTFS CLS CMD COLOR COMP COMPACT CONVERT COPY DATE '
+					 + 'DEL DIR DISKCOMP DISKCOPY DOSKEY ECHO ERASE EXIT FC FIND FINDSTR FORMAT FTYPE GRAFTABL '
+					 + 'HELP LABEL MD MKDIR MODE MORE MOVE PATH PAUSE POPD PRINT PROMPT PUSHD RD RECOVER REN RENAME REPLACE '
+					 + 'RMDIR SHIFT SORT START SUBST TIME TITLE TREE TYPE VER VERIFY VOL XCOPY';
+
+		var keywords = 'CON DEFINED DO ENABLEDELAYEDEXPANSION ENABLEEXTENSIONS ENDLOCAL FOR GOTO CALL IF IN ELSE NOT NUL REM SET SETLOCAL';
+
+		var variables = 'ALLUSERSPROFILE APPDATA CommonProgramFiles COMPUTERNAME ComSpec DATE FP_NO_HOST_CHECK HOMEDRIVE '
+					  + 'HOMEPATH LOGONSERVER NUMBER_OF_PROCESSORS OS Path PATHEXT PROCESSOR_ARCHITECTURE PROCESSOR_IDENTIFIER '
+					  + 'PROCESSOR_LEVEL PROCESSOR_REVISION ProgramFiles PROGS PROMPT SANDBOX_DISK SANDBOX_PATH SESSIONNAME '
+					  + 'SystemDrive SystemRoot TEMP TIME TMP USERDNSDOMAIN USERDOMAIN USERNAME USERPROFILE windir';
+
+		this.regexList = [
+			//
+			// REM Comments
+			//
+			{
+				regex: /(^::|rem).*$/gmi,
+				css: 'comments'
+			},
+
+			//
+			// "Strings"
+			// 'Strings'
+			// `Strings`
+			// ECHO String
+			//
+			{
+				regex: SyntaxHighlighter.regexLib.doubleQuotedString,
+				css: 'string' 
+			},
+			{
+				regex: SyntaxHighlighter.regexLib.singleQuotedString,
+				css: 'string' 
+			},
+			{
+				regex: /`(?:\.|(\\\`)|[^\``\n])*`/g,
+				css: 'string' 
+			},
+			{
+				regex: /echo.*$/gmi,
+				css: 'string'
+			},
+
+			//
+			// :Labels
+			//
+			{
+				regex: /^:.+$/gmi,
+				css: 'color3' 
+			},
+
+			//
+			// %Variables%
+			// !Variables!
+			//
+			{
+				regex: /(%|!)\w+\1/gmi,
+				css: 'variable'
+			},
+
+			//
+			// %%a variable Refs
+			// %1 variable Refs
+			//
+			{
+				regex: /%\*|%%?~?[fdpnxsatz]*[0-9a-z]\b/gmi,
+				css: 'variable' 
+			},
+
+			//
+			// keywords
+			//
+			{
+				regex: new RegExp(this.getKeywords(commands), 'gmi'),
+				css: 'keyword'
+			},
+
+			//
+			// keywords
+			//
+			{
+				regex: new RegExp(this.getKeywords(keywords), 'gmi'),
+				css: 'keyword' 
+			}
+		];
+	};
+
+	Brush.prototype	= new SyntaxHighlighter.Highlighter();
+	Brush.aliases	= ['bat', 'cmd', 'batch'];
+
+	SyntaxHighlighter.brushes.Ruby = Brush;
+
+	// CommonJS
+	typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+})();
---
+++ source/theme/scripts/SyntaxHighlighter/shBrushYaml.js
@@ -0,0 +1,34 @@
+;(function()
+{
+	// CommonJS
+	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+
+	function Brush()
+	{
+		// Contributed by Nicolas Perriault
+		
+		var constants	= '~ true false on off';
+
+		this.regexList = [
+			{ regex: SyntaxHighlighter.regexLib.singleLinePerlComments, css: 'comments' },		// comment
+			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },		// double quoted string
+			{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },		// single quoted string
+			{ regex: /^\s*([a-z0-9\._-])+\s*:/gmi,						css: 'variable' },		// key
+			{ regex: /\s?(\.)([a-z0-9\._-])+\s?:/gmi,					css: 'comments' },		// section
+			{ regex: /\s(@|:)([a-z0-9\._-])+\s*$/gmi,					css: 'variable bold' },	// variable, reference
+			{ regex: /\s+\d+\s?$/gm,									css: 'color2 bold' },	// integers
+			{ regex: /(\{|\}|\[|\]|,|~|:)/gm,							css: 'constants' },		// inline hash and array, comma, null
+			{ regex: /^\s+(-)+/gm,										css: 'string bold' },	// array list entry
+			{ regex: /^---/gm,											css: 'string bold' },	// category
+			{ regex: new RegExp(this.getKeywords(constants), 'gmi'),	css: 'constants' }		// constants
+			];
+	};
+	
+	Brush.prototype	= new SyntaxHighlighter.Highlighter();
+	Brush.aliases	= ['yaml', 'yml'];
+
+	SyntaxHighlighter.brushes.Yaml = Brush;
+
+	// CommonJS
+	typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+})();
